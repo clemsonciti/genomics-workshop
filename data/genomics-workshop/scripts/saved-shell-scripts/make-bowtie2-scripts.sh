11b9f8da8337c23c5aadf9dc4ad80a69d269ad42
masonaj157 <masonaj@knights.ucf.edu>
2018-06-13 17:14:05 -0400

    Update make-bowtie2-scripts.sh

--- data/genomics-workshop/scripts/saved-shell-scripts/make-bowtie2-scripts.sh
+++ data/genomics-workshop/scripts/saved-shell-scripts/make-bowtie2-scripts.sh
@@ -5,7 +5,7 @@ while read -r line
 do
 	export sraID=`echo $line | cut -d" " -f1`
 	export sample=`echo $line | cut -d" " -f2`
-	sed "s/TEMP_SRA/$sraID/g" scripts/bowtie2-aln.sh >scripts/temp.sh
-	sed "s/TEMP_SAMPLE/$sample/g" scripts/temp.sh >"scripts/bowtie2.$x.sh"
+	sed "s/TEMP_SRA/$sraID/g" ../scripts/bowtie2-aln.sh >scripts/temp.sh
+	sed "s/TEMP_SAMPLE/$sample/g" ../scripts/temp.sh >"scripts/bowtie2.$x.sh"
 	let x=(x+1)
 done < data/fileList.txt 
