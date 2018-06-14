fccc006e05d8cea12c730f38d9c1ef3ceb281a7f
masonaj157 <masonaj@knights.ucf.edu>
2018-06-14 13:07:46 -0400

    Update gatk-hapCall.sh

--- data/genomics-workshop/scripts/gatk-hapCall.sh
+++ data/genomics-workshop/scripts/gatk-hapCall.sh
@@ -6,22 +6,20 @@
 
 echo "START ------------------------------"
 
-module add java/1.8.0
+module add java/1.8.0 
 module load gnu-parallel
 module load GATK
 
 src=/home/$USER/genomics-workshop
 
-## Use gnu-parallel to use multiple cores
+### Use gnu-parallel to use multiple cores
 ### within one script
-
 cd $src/BamFiles
-cat list.txt | parallel -j 8 "java -jar $GATK \
+cat list.txt | parallel --verbose -j 8 "java -jar $GATK \
         -T HaplotypeCaller \
         -R $src/Reference/Ecoli_Ref.fa \
-        -ploidy 1 \
+	-ploidy 1 \
         -ERC GVCF -variant_index_type LINEAR -variant_index_parameter 128000 \
-        -I {}.sort.bam -o $src/SNPs/{}.gvcf"
+        -I {}.sort.bam -o $src/SNPs/{}.gvcf" 
 
 echo "FINISH ----------------------------"
-
